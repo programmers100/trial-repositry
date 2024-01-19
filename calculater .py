3d72ef1cdf62435dcc9ae35b1349b5d0183797db
Arman-demo <xentrepreneur2030@gmail.com>
2024-01-19 18:10:30 +0530

    removed all bugs

--- calculater .py
+++ calculater .py
@@ -1,11 +1,1 @@
-#hello its a trail repo
-#python
-a = input(int("enter your first number"))
-c = input("chose (*,+,-)")
-b= input(int("enter your second number"))
-
-if c =="*":
-    print(a*b)
-elif c =="+":
-    print(a+b)
-    
+if a ==b
